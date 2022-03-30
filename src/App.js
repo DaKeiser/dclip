af1dfd3d9bc994a6cd5918cb215153cd10e15d06
Saiakash <saiakash.konidena@gmail.com>
2022-03-31 02:09:03 +0530

    Added new post page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
 import React from 'react';
 import Container from './components/Container'
 import Page1 from './pages/Page1'
-import Page2 from './pages/Page2'
+import Post from './pages/Post'
 import UploadFile from "./pages/Upload";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Container />} />
       <Route path="/page1" element={<Page1 />} />
-      <Route path="/page2" element={<Page2 />} />
+      <Route path="/post" element={<Post />} />
       <Route path="/upload" element={<UploadFile />} />
     </Routes>
   </BrowserRouter>
--- src/pages/Page2.js
+++
@@ -1,9 +0,0 @@
-import Container from '../components/Container'
-
-export default function Index() {
-  return (
-    <Container>
-      Page2
-    </Container>
-  )
-}
