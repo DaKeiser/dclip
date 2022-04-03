150a333873c836224d71632bd2d698b3430568e4
Saiakash <saiakash.konidena@gmail.com>
2022-04-04 01:04:45 +0530

    Added hyperlink to heading

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,14 @@
 import { Box, Heading } from "@chakra-ui/layout"
-import { useColorMode } from "@chakra-ui/react";
+import { useColorMode, Link } from "@chakra-ui/react";
 import { primaryTextColor } from '../styles/darkMode';
 
 const Logo = () => {
     const { colorMode } = useColorMode()
     return (
         <Box p={8} pb={4}>
-            <Heading color={primaryTextColor[colorMode]}>dClip.</Heading>
+            <Link href="/">
+                <Heading color={primaryTextColor[colorMode]}>dClip.</Heading>
+            </Link>
         </Box>
     )
 };
